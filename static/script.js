40635cb99deb524e4286d574c1e8d150a15a85bb
Ishita Singhal <75316315+ishita-singhal@users.noreply.github.com>
2023-08-04 13:41:31 +0530

    Updated Css and js

---
+++ static/script.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.getElementById("myModal");
+    const submitButton = document.getElementById("submitButton");
+    const closeModal = document.getElementById("closeModal");
+    const goBackButton = document.getElementById("goBackButton");
+
+    submitButton.addEventListener("click", function (event) {
+        event.preventDefault(); // Prevent form submission
+        modal.style.display = "block";
+    });
+
+    closeModal.addEventListener("click", function () {
+        modal.style.display = "none";
+    });
+
+    goBackButton.addEventListener("click", function () {
+        modal.style.display = "none";
+    });
+});
+
+
+//timer
+// Get the necessary elements
+const countdownElement = document.getElementById("countdown");
+const submitButton = document.getElementById("submitButton");
+const questionnaireForm = document.getElementById("questionnaireForm");
+
+let timeRemaining = 1800; // 30 minutes in seconds
+
+function updateCountdown() {
+    const minutes = Math.floor(timeRemaining / 60);
+    const seconds = timeRemaining % 60;
+    countdownElement.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
+function submitForm() {
+    questionnaireForm.submit();
+}
+
+const countdownInterval = setInterval(function () {
+    if (timeRemaining > 0) {
+        timeRemaining--;
+        updateCountdown();
+    } else {
+        clearInterval(countdownInterval);
+        updateCountdown();
+        submitForm();
+    }
+}, 1000);
