6a7f45617f844e6e9c692cb0fd38492b51ec256a
Anish Vantagodi <anishvv212002@gmail.com>
2023-08-23 22:03:09 +0530

    evaluate pending

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,session
 import pdfplumber
 from langchain.document_loaders.base import Document
 from langchain.text_splitter import CharacterTextSplitter
@@ -8,10 +8,12 @@ from langchain.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain.llms import OpenAI
 import yagmail
+import Levenshtein
 
 os.environ["OPENAI_API_KEY"] = "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"
 
 app = Flask(__name__)
+app.secret_key = "1234"
 
 # Set the path for the resumes folder
 UPLOAD_FOLDER = 'resumes'
@@ -41,7 +43,7 @@ def send_congratulatory_email(candidate_email):
     from_email = "404found3@gmail.com"  # Your email address
     password = "nnrpejvnjwdffdwl"     # Your email password or app password
     subject = "Congratulations on Clearing the Resume Screening Test"
-    message = "Dear Candidate,\n\nCongratulations! You have successfully cleared the resume screening test for the position. You are now invited to the first-round interview.\n\nPlease log in to our website using the provided password below:\n\nPassword: YourPassword123\n\nLink: http://127.0.0.1:5000/next-round\n\nWe look forward to seeing you for the interview.\n\nBest regards,\n404Found"
+    message = "Dear Candidate,\n\nCongratulations! You have successfully cleared the resume screening test for the position. You are now invited to the first-round interview.\n\nPlease log in to our website using the provided password below:\n\nPassword: YourPassword123\n\nLink: http://ec2-13-127-117-112.ap-south-1.compute.amazonaws.com:8080/next-round\n\nWe look forward to seeing you for the interview.\n\nBest regards,\n404Found"
 
     try:
         yag = yagmail.SMTP(from_email, password)
@@ -63,6 +65,44 @@ def send_notselected_email(candidate_email):
     except Exception as e:
         print("Error sending email:", e)
 
+def send_hr_email(our_mail , candidate_email):
+    from_email = "404found3@gmail.com"  # Your email address
+    password = "nnrpejvnjwdffdwl"     # Your email password or app password
+    subject = "Candidate has been selected"
+    message = f"{candidate_email} has been selected"
+
+    try:
+        yag = yagmail.SMTP(from_email, password)
+        yag.send(our_mail, subject, message)
+        print("HR email sent successfully!")
+    except Exception as e:
+        print("Error sending email:", e)
+        
+def send_notselection_email(candidate_email):
+    from_email = "404found3@gmail.com"  # Your email address
+    password = "nnrpejvnjwdffdwl"     # Your email password or app password
+    subject = " Update Regarding Your Interview"
+    message = "We hope this message finds you well. Thank you for your interest. We appreciate the time and effort you invested in the interview process.\n\nAfter careful consideration, we regret to inform you that you have not been selected to move forward to the next stage of the selection process. While your qualifications are impressive, we have chosen to proceed with other candidates who closely match the specific requirements for the role.\n\nWe want to express our gratitude for your interest in joining [Company Name]. Your application and interview were valued, and we encourage you to consider future opportunities with us.\n\nWe wish you the best in your continued job search and professional endeavors. If you have any questions or would like feedback on your interview, please feel free to reach out to us.\n\nThank you again for considering our company. We appreciate your understanding and wish you success in your future endeavors.\n\nBest regards,\n404Found"
+
+    try:
+        yag = yagmail.SMTP(from_email, password)
+        yag.send(candidate_email, subject, message)
+        print("Not selected email sent successfully!")
+    except Exception as e:
+        print("Error sending email:", e)
+        
+def send_selection_email(candidate_email):
+    from_email = "404found3@gmail.com"  # Your email address
+    password = "nnrpejvnjwdffdwl"     # Your email password or app password
+    subject = "Invitation to Final Round Face-to-Face Interview"
+    message = "Congratulations! We are pleased to inform you that you have successfully cleared the first-round interview test for the position. Your performance and qualifications have impressed us.\n\nWe would like to proceed to the next stage of the selection process by inviting you to a final round of face-to-face interview. Our team is excited to learn more about you and discuss your potential contributions to our organization.\n\nExpect to hear from our team soon to coordinate the details of the upcoming interview. We are eager to meet you in person and explore the possibility of you joining our team.\n\nThank you for your interest in our company, and once again, congratulations on your achievement.\n\nBest regards,\n404Found"
+
+    try:
+        yag = yagmail.SMTP(from_email, password)
+        yag.send(candidate_email, subject, message)
+        print("Congratulations email sent successfully!")
+    except Exception as e:
+        print("Error sending email:", e)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -122,11 +162,12 @@ def index():
 
     return render_template('index.html')
 
+
 @app.route('/next-round', methods=['GET', 'POST'])
 def next_round():
     if request.method == 'POST':
         entered_password = request.form.get('password')
-        if entered_password == 'YourPassword123':
+        if entered_password == '1234':
             cv_file = request.files.get('cv')
 
             if cv_file:
@@ -152,26 +193,41 @@ def next_round():
 
                 # Generate the list of questions using the OpenAI API
                 query = """
-                can you ask 5 questions for this candidates interview based on this resume , machine learning role and his skills , make the questions such that it can be answered in one line"""
+                Based on the resume, generate a mix of interview questions related to the candidate's projects, domain skills, and general skills. Provide answers for the questions. Ensure that the questions generated do not include the phrases 'Domain-related question' and 'Project-related question and also they are easy to attend , keep only 6 question , start each question by adding the word 'Question:'"""
                 result = qa(query)
-                ans = result['result']
-                input_string = ans
+                ques = result['result']
+                input_string = ques
+                
+                query_email = "candidate's email?"
+                result_email = qa(query_email)
+                ans_email = result_email['result']
+                
+                # Split the input string into a list of lines
+                lines = input_string.split('\n')
 
-                # Split the input string into a list of questions
-                questions_list = input_string.split('\n')
+                # Initialize arrays to store questions and expected answers
+                question_array = []
+                expected_answer_array = []
 
-                # Remove the '\n' characters from each question
-                questions_list = [question.strip() for question in questions_list if question.strip()]
+                # Iterate through the lines to extract questions and expected answers
+                for line in lines:
+                    line = line.strip()
+                    if line.startswith("Question"):
+                        question_array.append(line[len("Question"):].strip())  
+                    elif line.startswith("Answer:"):
+                        expected_answer_array.append(line[len("Answer"):].strip())
 
-                # Pass the questions_list and cv_filename to the template
-                return render_template('question.html', questions_list=questions_list, cv_filename=cv_filename)
+                print(expected_answer_array)
+                session['expected_answer_array'] = expected_answer_array
+                session['ans_email'] = ans_email
+                # Pass the question_array and expected_answer_array to the template
+                return render_template('question.html', question_array=question_array, expected_answer_array=expected_answer_array, cv_filename=cv_filename)
             else:
                 return render_template('next_round.html', error_message='Please upload a resume.')
         else:
             return render_template('next_round.html', error_message='Incorrect password. Please try again.')
 
     return render_template('next_round.html')
-
 
 @app.route('/question')
 def question():
@@ -184,6 +240,39 @@ def evaluate():
 @app.route('/go-back')
 def go_back():
     return redirect(url_for('index'))
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    user_answer_array = []
+    expected_answer_array = session.get('expected_answer_array', [])
+    ans_email = session.get('ans_email','')
+    print(ans_email)
+    print(expected_answer_array)
+    # Loop through the form fields and retrieve user answers
+    for i in range(1, 7):  
+        user_answer = request.form.get(f'answer{i}')
+        user_answer_array.append(user_answer)
+
+    correct_count = 0
+    threshold = 0.7  # Adjust as needed
+
+    for user_answer, expected_answer in zip(user_answer_array, expected_answer_array):
+        levenshtein_distance = Levenshtein.distance(user_answer.lower(), expected_answer.lower())
+        max_length = max(len(user_answer), len(expected_answer))
+        similarity_ratio = 1 - (levenshtein_distance / max_length)
+
+        if similarity_ratio >= threshold:
+            correct_count += 1
+
+    accuracy = correct_count / len(user_answer_array) * 100
+    print(accuracy)
+    if accuracy>65:
+        send_selection_email(ans_email)
+        send_hr_email('404found3@gmail.com',ans_email)
+    else:
+        send_notselection_email(ans_email)
+    # Render the evaluate.html template and pass the accuracy
+    return render_template('evaluate.html', accuracy=accuracy)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8080)
